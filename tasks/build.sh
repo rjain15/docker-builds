2a4e55e562da6da00809ab0a32323cf222cef1b6
Rajesh Jain <rajesh@xclo.io>
2018-11-02 00:16:22 -0400

    Debugging...

--- tasks/build.sh
+++ tasks/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
+$REPO = $1
+$TOKEN = $2
+
 echo "Reps" ... $REPO
-echo "Token " .. $TOKEN 
+echo "Token " .. $TOKEN
 echo curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/rjain15/$REPO/trigger/$TOKEN/
